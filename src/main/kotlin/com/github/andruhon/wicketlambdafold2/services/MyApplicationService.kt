f5e70d758eb8c3c521027d944fc1a3a67ce1c388
GitHub Action <action@github.com>
2021-03-10 00:12:50 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/andruhon/wicketlambdafold2/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.andruhon.wicketlambdafold2.services
+
+import com.github.andruhon.wicketlambdafold2.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/andruhon/wicketlambdafold2/services/MyProjectService.kt
+++ src/main/kotlin/com/github/andruhon/wicketlambdafold2/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.andruhon.wicketlambdafold2.services
 
+import com.github.andruhon.wicketlambdafold2.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
